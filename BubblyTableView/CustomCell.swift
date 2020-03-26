f4f87a2b72afc554a2809e86ac0d652e8aab00c1
Tanner <tanner@Tanners-MacBook-Pro.local>
2020-03-26 12:38:34 -0500

    first commit

---
+++ BubblyTableView/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  BubblyTableView
+//
+//  Created by Tanner on 3/16/20.
+//  Copyright © 2020 Tanner. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BubblyTableView/PostClass.swift
@@ -0,0 +1,9 @@
+//
+//  PostClass.swift
+//  BubblyTableView
+//
+//  Created by Tanner on 3/16/20.
+//  Copyright © 2020 Tanner. All rights reserved.
+//
+
+import Foundation
